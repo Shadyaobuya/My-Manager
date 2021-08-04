0303f929f8e465bd54fd9083c633b9f69cdf6c78
Shadyaobuya <shadyaobuyagard@gmail.com>
2021-08-04 17:45:56 +0300

    added allowed host

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q%j7&j*2l48qo%0z%lu+yb#cco!2pq8sssz*j-4lem4b8rvb5)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['my-manager-to-do-app.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-manager-to-do-app.herokuapp.com']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
